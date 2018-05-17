edbb0eedf6b14547b70e85703636093108710858
quemanting <694682740@qq.com>
2018-05-17 17:18:45 +0800

    序列号拼接2

--- src/app/waiting/waiting.component.ts
+++ src/app/waiting/waiting.component.ts
@@ -17,7 +17,7 @@ export class WaitingComponent implements OnInit {
     private route: ActivatedRoute,
   ) {}
   returnBtn(){
-    if(typeof MainWindow !='undefined'){MainWindow.backToMain()}
+    //if(typeof MainWindow !='undefined'){MainWindow.backToMain()}
   }
   cliclCard(item){
     item.type = this.type;
@@ -33,18 +33,16 @@ export class WaitingComponent implements OnInit {
     const serialNumber = this.route.params["value"].serialNumber;
     this.type = type;
     this.serialNumber = serialNumber;
-    if(serialNumber){
-      //const params = new HttpParams().set('serialNumber','GB8ZAWGE0A');
+    if(serialNumber!=''){
       const params = new HttpParams().set('serialNumber',serialNumber);
-    }else{
-      alert('平板序列号未传入');
-    }
-    this.http.get('/case/list/room/person',{params})
+      this.http.get('/case/list/room/person',{params})
       .subscribe(res=>{//success
         this.cardDataList = (res as any).data;
       },error=>{
         console.log(error)
       });
-   }
-
+    }else{
+      alert('平板序列号未传入');
+    }
+  }
 }
